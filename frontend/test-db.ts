33917e41249c20348a5eaa2e9bd2829f87076644
Hop-Syder <daoudaabassichristian@gmail.com>
2026-06-17 08:36:12 +0100

    chore: remove test-db.ts

--- frontend/test-db.ts
+++
@@ -1,6 +0,0 @@
-import prisma from './src/lib/db';
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log(JSON.stringify(users, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
